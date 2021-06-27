b662e844a67805aec15b8194c07490f2812285b9
douglasolv <douglas4030@gmail.com>
2021-06-26 21:28:39 -0300

    refactor: detach controller

--- src/handlers/graphql.rs
+++ src/handlers/graphql.rs
@@ -1,62 +1,24 @@
+use crate::controllers::schema::{Context, Schema};
 use crate::Pool;
-use diesel::RunQueryDsl;
-use juniper::{EmptySubscription, FieldError, FieldResult, RootNode};
-use std::sync::Arc;
+use actix_web::{web, HttpResponse};
+use juniper::http::{playground::playground_source, GraphQLRequest};
 
-use crate::models::user::{User, NewUser, CreateUserInput};
+pub async fn graphql_playground() -> HttpResponse {
+    let html = playground_source("/graphql", None);
 
-#[derive(Clone)]
-pub struct Context {
-    pub pool: Arc<Pool>,
+    HttpResponse::Ok()
+        .content_type("text/html; charse=utf-8")
+        .body(html)
 }
 
-impl juniper::Context for Context {}
+pub async fn graphql(
+    data: web::Json<GraphQLRequest>,
+    schema: web::Data<Schema>,
+    pool: web::Data<Pool>,
+) -> HttpResponse {
+    let pool = pool.into_inner();
+    let context = Context { pool };
+    let res = data.execute(&schema, &context).await;
 
-pub struct Query;
-
-#[juniper::graphql_object(Context = Context)]
-impl Query {
-    async fn api_version() -> &str {
-        "1.0"
-    }
-    async fn users(context: &Context) -> Vec<User> {
-        let conn = context.pool.get().unwrap();
-        User::all(&conn)
-    }
-}
-
-pub struct  Mutation;
-
-#[juniper::graphql_object(Context = Context)]
-impl Mutation {
-    async fn create_user(context: &Context, user: CreateUserInput,) -> FieldResult<User>  {
-        use crate::schema::users;
-
-        let user = NewUser{
-            name: &user.name,
-            email: &user.email,
-            password: &user.password,
-        };
-        let conn = context.pool.get().unwrap();
-
-       let ress = diesel::insert_into(users::table)
-       .values(&user).get_result(&conn);
-
-       match ress {
-           Ok(t) => Ok(t),
-           Err(e) => FieldResult::Err(FieldError::from(e))
-       }
-
-    }
-}
-
-
-pub type Schema = RootNode<'static, Query, Mutation, EmptySubscription<Context>>;
-
-pub fn create_schema() -> Schema {
-    Schema::new(
-        Query,
-        Mutation,
-        EmptySubscription::new(),
-    )
+    HttpResponse::Ok().json(res)
 }
--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -1,42 +1,20 @@
-mod graphql;
-
-use crate::Pool;
+use crate::controllers::schema;
 use actix_web::{web, HttpResponse};
-use graphql::{create_schema, Context, Schema};
-use juniper::{http::{GraphQLRequest, playground::playground_source}};
+
+mod graphql;
 
 async fn health() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
 pub fn app_config(config: &mut web::ServiceConfig) {
-    let schema = create_schema();
+    let schema = schema::create_schema();
     config
         .data(schema)
         .service(web::resource("/").route(web::get().to(health)))
         .service(
             web::resource("/graphql")
-                .route(web::post().to(graphql))
-                .route(web::get().to(graphiql)),
+                .route(web::post().to(graphql::graphql))
+                .route(web::get().to(graphql::graphql_playground)),
         );
-}
-
-async fn graphiql() -> HttpResponse {
-    let html = playground_source("/graphql", None);
-
-    HttpResponse::Ok()
-        .content_type("text/html; charse=utf-8")
-        .body(html)
-}
-
-async fn graphql(
-    data: web::Json<GraphQLRequest>,
-    schema: web::Data<Schema>,
-    pool: web::Data<Pool>,
-) -> HttpResponse {
-    let pool = pool.into_inner();
-    let context = Context { pool };
-    let res = data.execute(&schema,&context).await;
-
-    HttpResponse::Ok().json(res)
 }
