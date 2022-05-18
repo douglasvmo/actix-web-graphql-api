0d3108ab81a480ffd22a60a2c22781ba47573752
Douglas Oliveira <douglas4030@gmail.com>
2022-05-17 23:51:40 -0300

    fix: change users table

--- migrations/2021-05-06-010300_create_users/up.sql
+++ migrations/2021-05-06-010300_create_users/up.sql
@@ -4,14 +4,13 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE TABLE IF NOT EXISTS users (
   id uuid DEFAULT uuid_generate_v4(),
   name VARCHAR(255) NOT NULL,
-  email VARCHAR(80) NOT NULL,
-  cpf_cnpj VARCHAR(14) NOT NULL,
+  email VARCHAR(80) UNIQUE NOT NULL,
+  cpf_cnpj VARCHAR(14) UNIQUE NOT NULL,
   password VARCHAR(128) NOT NULL,
   role_id INTEGER,
   active BOOLEAN NOT NULL DEFAULT 'f',
   verification_payload VARCHAR(8),
   created_at TIMESTAMP NOT NULL DEFAULT NOW(),
   updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
-  UNIQUE(email, cpf_cnpj),
   PRIMARY KEY (id)
 );
